661f941b239fe64ba66ef7a47ed33956fb9f81ee
eracknaphobia <eracknaphobia@hotmail.com>
2015-03-11 14:12:40 -0400

    Fixes / Updates

--- supercross.py
+++ supercross.py
@@ -1,5 +1,5 @@
 import sys
-import xbmcplugin, xbmcgui, xbmcaddon
+import xbmc, xbmcplugin, xbmcgui, xbmcaddon
 import re, os, time
 import urllib, urllib2
 import json
@@ -7,105 +7,101 @@ import json
 ROOTDIR = xbmcaddon.Addon(id='plugin.video.amaproracing').getAddonInfo('path')
 FANART = ROOTDIR+'/images/fanart_supercross.jpg'
 ICON = ROOTDIR+'/images/icon_supercross.png'
-MAIN_URL = 'http://www.supercrossonline.com'
+LIVE_FANART = 'http://www.supercrosslive.com/sites/default/files/Hero_slide_RDL.jpg'
 
 class supercross():
 
-    def CATEGORIES(self):
-        self.addDir('Archive','/supercross',201,ICON)
+    def CATEGORIES(self):        
+        self.addDir('Race Day Live','/supercross/live',202,ICON,LIVE_FANART)        
+        self.addDir('Race Day Archive','/supercross/archive',203,ICON)
+        self.addDir('View Supercross Youtube Channel','/supercross/youtube',201,ICON)                
 
 
-    def ARCHIVE(self):
-        url = MAIN_URL+'/supercrosslive/'                
-        req = urllib2.Request(url)
-        req.add_header('User-Agent', ' Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-        response = urllib2.urlopen(req)
-        link=response.read()
-        response.close()        
-        link = link.replace('\n',"")
-        start = link.index('<strong>Archived Streams:</strong>')
-        end = link.index('</p>',start)    
-        link = link[start:end]
-        link = link.replace(' target="_blank"','')
-        print link
-        #<a href="http://www.supercrossonline.com/News/SupercrossLive/2014/04/24/1/"> East Rutherfrod Pre-Race Press Conference - Thursday, April 24</a>
-        #match = re.compile('<a href="(.+?)" target="_blank">Supercross LIVE!(.+?)<').findall(link)    
-        #if not match: 
-        match = re.compile('<a href="(.+?)">(.+?)<').findall(link)    
+    def RACE_DAY_LIVE(self):                               
+
+        #Attempt to get the Live Stream
+        try:            
+            json_source = self.GET_LIVESTREAM_INFO()
+            self.LIVESTREAM_LINK(str(json_source['upcoming_events']['data'][0]['id']))
+        except:
+            #Get the next live stream date
+            url = 'http://www.supercrosslive.com/race-day-live'
+            req = urllib2.Request(url) 
+            req.add_header('User-Agent', ' Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36') 
+            #req.add_header('Referer', 'http://www.promotocross.com/motocross/live')
+            response = urllib2.urlopen(req) 
+            html = response.read()
+            start = html.find('<h2> Race Day Live presented')
+            end = html.find('</h2>', start)
+            next_stream = html[start+5:end]
             
-        for link, location in match: 
-            page_url = self.GET_VIDEO_PAGE(link)
-            if page_url != '':                
-                self.GET_MP4(page_url,location)            
-        return
-
-    def GET_VIDEO_PAGE(self,url):
-        #Go to link and Find This
-        #<iframe width="640" scrolling="no" height="360" frameborder="0" src="http://new.livestream.com/accounts/1543541/events/2641627/videos/38811042/player?autoPlay=false&amp;height=360&amp;mute=false&amp;width=640"></iframe></p>
-        if url.startswith('/'):
-                url = MAIN_URL + url
-
-        print "HERE URL===" + url
-        req = urllib2.Request(url)
-        req.add_header('User-Agent', ' Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-        response = urllib2.urlopen(req)
-        link=response.read()
-        response.close()        
-        link = link.replace('\n',"")
-
-        ##############################################
-        #Skip past google iframe crap
-        ##############################################
-        start = link.find('<div id="menucontainer">')
-        end = link.find('</body>')
-        link = link[start:end]        
-        ##############################################
-
-        start_str = '<iframe width="640" scrolling="no" height="360" frameborder="0" src="'
-        start = link.find(start_str)        
-        if start == -1:                    
-            start_str = '<iframe src="'
-            start = link.find(start_str)   
+            self.addLink(next_stream,'',next_stream, LIVE_FANART)
             
-        end = link.find('?autoPlay=false&amp;height=360&amp;mute=false&amp;width=640"',start)    
-        if start != -1 and end != -1:
-            link = link[start+len(start_str):end]
-        else:
-            link = ''
-        return link
-           
-
-    def GET_MP4(self,url,location):
-        #print 'HERE"S my link==='+url
-        req = urllib2.Request(url)
-        req.add_header('User-Agent', ' Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-        response = urllib2.urlopen(req)
-        link=response.read()
-        response.close()    
-
-        start_str = '"progressive_url_hd":"'       
-        start = link.find(start_str)
-
-        #if start == -1:
-            #start_str = '"progressive_url":"'       
-            #start = link.find(start_str)
-
-        end = link.find('"',start+len(start_str))  
-        if start != -1 and end != -1:
-            video_url = link[start+len(start_str):end]
-
-            start_str = '"thumbnail_url":"'
-            start = link.find(start_str)
-            end = link.find('"',start+len(start_str)) 
-            image_url = link[start+len(start_str):end]
+        finally:
+            pass
+        
             
-            self.addLink(location,video_url,location,image_url) 
+    def RACE_DAY_ARCHIVE(self):
+        try:
+            json_source = self.GET_LIVESTREAM_INFO()
+            #Load all past events
+            for past_event in json_source['past_events']['data']:            
+                self.LIVESTREAM_LINK(str(past_event['id']))
+        except:
+            pass
+
+
+    def GET_LIVESTREAM_INFO(self):
+        #Get Supercross livestream.com info
+        url = 'http://new.livestream.com/api/accounts/1543541'
+        req = urllib2.Request(url) 
+        req.add_header('User-Agent', ' Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36') 
+        #req.add_header('Referer', 'http://www.promotocross.com/motocross/live')
+        response = urllib2.urlopen(req)        
+        #data_sources = response.read()
+        json_source = json.load(response)
+        response.close()  
+
+        return json_source
+
+
+    def LIVESTREAM_LINK(self,event_id):
+        url = 'http://new.livestream.com/api/accounts/1543541/events/'+event_id+'/feed.json?&filter=video'
+        #print url
+        try:
+            req = urllib2.Request(url) 
+            req.add_header('User-Agent', ' Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36')             
+            response = urllib2.urlopen(req)                    
+            json_source = json.load(response)
+            response.close()
+
+            name = json_source['data'][0]['data']['caption']
+            img_url = json_source['data'][0]['data']['thumbnail_url']
+
+            #Attempt to get the HD feed, if not try for the SD
+            try:
+                live_url = json_source['data'][0]['data']['progressive_url_hd']
+                self.addLink(name, live_url, name, img_url)
+            except:
+                live_url = json_source['data'][0]['data']['progressive_url']
+                self.addLink(name, live_url, name, img_url)
+            finally:
+                pass
+        except:
+            pass
+
+
+    def SUPERCROSS_YOUTUBE_CHANNEL(self):        
+        win = str(xbmcgui.getCurrentWindowId())
+        xbmc.executebuiltin('ActivateWindow('+win+',plugin://plugin.video.youtube/user/SupercrossLive/,return)')
+
+
 
     def addLink(self,name,url,title,iconimage):
         ok=True
-        liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage,)
+        liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
         liz.setProperty('fanart_image',FANART)
-        
+        liz.setProperty("IsPlayable", "true")
         liz.setInfo( type="Video", infoLabels={ "Title": title } )
         ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
         return ok
@@ -116,6 +112,10 @@ class supercross():
         u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
         liz=xbmcgui.ListItem(name, iconImage=ICON, thumbnailImage=iconimage)
         liz.setInfo( type="Video", infoLabels={ "Title": name } )
-        liz.setProperty('fanart_image', FANART)
+        if fanart != None:
+            liz.setProperty('fanart_image', fanart)
+        else:
+            liz.setProperty('fanart_image', FANART)
         ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)    
         return ok
+
